5d54777f50661c2c29f4d10ca6708495a9a8b2ec
Rebeca Santos <rcks.santos@gmail.com>
2020-03-11 10:27:12 -0300

    terceiro commit do code along de ecommerce com React.

--- src/context.js
+++ src/context.js
@@ -9,9 +9,12 @@ class ProductProvider extends Component {
     state = {
         products:[],
         detailProduct: detailProduct,
-        cart: [],
+        cart: storeProducts,
         modalOpen: false,
         modalProduct:detailProduct, 
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     };
     componentDidMount() {
         this.setProducts();
@@ -62,6 +65,18 @@ class ProductProvider extends Component {
             return {modalOpen:false}
         })
     }
+    increment = (id) => {
+        console.log("mais um")
+    }
+    decrement = (id) => {
+        console.log("menos um")
+    }
+    removeItem = (id) => {
+        console.log("remove item")
+    }
+    clearCart = () => {
+        console.log("carrinho vazio")
+    }
     render() {
         return (
             <ProductContext.Provider value={{
@@ -69,7 +84,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart:this.addToCart,
                 openModal:this.openModal,
-                closeModal:this.closeModal
+                closeModal:this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {this.props.children}
             </ProductContext.Provider>
